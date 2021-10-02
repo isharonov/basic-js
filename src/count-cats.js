f418cb1679f9777ee5ad95fc2e9db398a2d9fbfa
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2021-10-02 16:45:48 +0300

    feat: resolve task Count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,3 @@
-import { NotImplementedError } from '../extensions/index.js';
-
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
@@ -14,7 +12,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let cat = '^^';
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++){
+    for (let j = 0; j < matrix[i].length; j++){
+      if (matrix[i][j] === cat) count++;
+    }
+  }
+  return count;
 }
